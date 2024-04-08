7b5a1b5193181c26a07e02f12677b6a1bf81f98d
13396565428 <redfish_zly@163.com>
2024-04-08 23:21:18 +0800

    fadsf

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production'? '/vue-project/': '/',
+  publicPath: '/vue-project/',
   plugins: [
     vue(),
   ],
