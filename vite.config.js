4459c13dc043c230d65c769361b717e0336ac6a8
13396565428 <redfish_zly@163.com>
2024-04-08 23:13:10 +0800

    fasdf

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'? '/vue-project/': '/',
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
