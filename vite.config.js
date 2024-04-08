8fdc65a8dcd6faf3f1f05803343019dc43a88963
13396565428 <redfish_zly@163.com>
2024-04-08 23:23:57 +0800

    fadf

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   lintOnSave: false,
-  publicPath: '/vue-project/',
+  publicPath: '/vue-project',
   plugins: [
     vue(),
   ],
