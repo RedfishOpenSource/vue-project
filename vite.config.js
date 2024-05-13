9ab580767528e779a9a7ecdcbfeb8b5590f146a7
13396565428 <redfish_zly@163.com>
2024-05-13 23:29:00 +0800

    '自动同步'

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dns from "dns"
+dns.setDefaultResultOrder("verbatim")
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // 监听所有IP地址，允许局域网访问
+  },
   base:'vue-project',
   plugins: [
     vue(),
